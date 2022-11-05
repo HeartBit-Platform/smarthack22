c0a8fec5f122392ddbde9d6072b74462f087ef0e
Adelin Popa <adelin.popa@RO-QM69QJ34JF.local>
2022-11-05 16:11:59 +0200

    Update README.md

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@
 export default function HomePage(){
     return ( 
         <div>
-            <h1> Test</h1>
-            <h3>Test2</h3>
+            <h1> UAU E IN TIMP REAL</h1>
+            <h3> sunt sefu adox</h3>
         </div>
     )
 }

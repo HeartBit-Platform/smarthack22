7e5436a18668fe3d4ee11255857b7b403e6671f6
Mihai Dinu <dinu.mihai.dinel@gmail.com>
2022-11-05 19:40:35 +0200

    Register Form test

--- app/page.js
+++ app/page.js
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
 export default function HomePage(){
     return ( 
-<<<<<<< HEAD
         <div>
-<<<<<<< HEAD
-            
-=======
->>>>>>> 38494a8370cf21c101eab4d90f5374a8b2d4af11
-=======
->>>>>>> 3e7a902d0a3324c07251d377e9ad852d81fb8a7c
+
 
         </div>
     )
 }
-=======
-import Headerr from '../components/Headerr.js';
-export default function HomePage() {
-    return <></>;
-}
->>>>>>> 4c767b95644a3369c35a959e6fa71127f90a5e82

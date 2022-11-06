c3d6006b515cacffec32835c32501216b34d11b3
Cipppp <pirvuciprian37@gmail.com>
2022-11-06 10:58:34 +0200

    Avatar updated

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Cards from '../components/Cards';
+import Image from 'next/image';
 
 export default function Home() {
     return (
@@ -7,14 +8,21 @@ export default function Home() {
                 className="background h-screen w-screen items-center justify-center"
                 id="home"
             >
-                <div className="grid grid-rows-2 p-20 place-items-center">
-                    <h1 className="font-mono font-extrabold  text-5xl">
-                        MecaniX
-                    </h1>
-                    <p className="pt-[5%]">
-                        Bine ai venit! MecaniX este platforma unde poți să îți
-                        creezi un site de prezentare pentru service-ul tău auto.
-                    </p>
+                <div className="grid grid-cols-2 p-20 place-items-center">
+                    <div>
+                        <h1 className="font-mono font-extrabold  text-5xl">
+                            MecaniX
+                        </h1>
+                        <p className="pt-[5%]">
+                            Bine ai venit! MecaniX este platforma unde poți să
+                            îți creezi un site de prezentare pentru service-ul
+                            tău auto.
+                        </p>
+                    </div>
+
+                    <div>
+                        <img src="/../public/Doodless.png" className="p-3" />
+                    </div>
                 </div>
             </div>
 

28e74f137a410905e8ffb2f0e2a5e58cdca882e8
Emzy13 <emilia_teo2002@yahoo.com>
2022-11-06 11:44:10 +0200

    Final demo

--- app/user/page.js
+++ app/user/page.js
@@ -1,18 +1,19 @@
 import Headerr from '../../components/Headerr.js';
 import Lista from '../../components/Lista';
-import Image from 'next/image';;
+import Image from 'next/image';
+
 export default function HomePage() {
     return (
         <>
             <div className="grid grid-cols-5 grid-rows-2 ">
                 <div className="col-span-2 row-span-2 flex-start">
-                    <div className="flex flex-col justify-center items-start">
-                        <div className="flex p-2 font-josefin font-bold border-2 border-black w-[16rem] items-center justify-center">
-                            <img
-                                class="p-1 w-16 h-16 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 self-center "
-                                src={image}
-                                alt=""
-                            />
+                    <div className="flex flex-col justify-center items-start ">
+                        <div className="relative hidden lg:inline-grid mt-0 h-16 w-16 grid-auto-flow:row cursor-pointer ml-20  items-center justify-center">
+                        <Image
+                            src="/../public/Doodles.png"
+                            layout="fill"
+                            className="p-3"
+                        />
                         </div>
                         <h1 className="flex field p-5 font-josefin font-bold border-2 border-black w-[16rem]">
                             <span className="flex flex-center">
@@ -41,6 +42,7 @@ export default function HomePage() {
                     <div>
                         <h1 className="font-bold font-xl">Account Settings</h1>
                     </div>
+
                 </div>
             </div>
         </>
